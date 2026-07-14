e8ede3cbfb05aa6cbd203a38cedfd14e54035caa
Lương Việt Hoàng <tcm4095@gmail.com>
2026-07-15 03:08:04 +0700

    replace `lib.throwIfNot`

--- nix-index-cache.nix
+++ nix-index-cache.nix
@@ -1,6 +1,5 @@
 {
   fetchzip,
-  lib,
   stdenv,
   zstd,
 
@@ -9,15 +8,15 @@
 let
   generated = builtins.fromJSON (builtins.readFile ./generated.json);
 in
-lib.throwIfNot (generated.hashes ? ${system}) "nix-index-cache has no cache for ${system}" fetchzip
-  {
-    url = "https://github.com/player131007/nix-index-cache/releases/download/${generated.release}/${system}.tar.zst";
-    hash = generated.hashes.${system};
-    stripRoot = false;
+assert generated.hashes ? ${system} || throw "nix-index-cache: no cache for ${system}";
+fetchzip {
+  url = "https://github.com/player131007/nix-index-cache/releases/download/${generated.release}/${system}.tar.zst";
+  hash = generated.hashes.${system};
+  stripRoot = false;
 
-    nativeBuildInputs = [ zstd ];
-    derivationArgs = {
-      __structuredAttrs = true;
-      unsafeDiscardReferences.out = true;
-    };
-  }
+  nativeBuildInputs = [ zstd ];
+  derivationArgs = {
+    __structuredAttrs = true;
+    unsafeDiscardReferences.out = true;
+  };
+}
